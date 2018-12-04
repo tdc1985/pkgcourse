17b79c5de62807763a90548d9a7a3e02622b359f
unknown <tdcarter1985@gmail.com>
2018-12-04 11:28:12 +0000

    Added Roxygen header to statsSummary

--- R/statsummary.R
+++ R/statsummary.R
@@ -1,12 +1,21 @@
+#' statSummary
+#'
+#' @param x a \code{numeric} vector for summarising
+#' @param na.rm a \code{logical} value stating whether to remove \code{NA} values; defaults to \code{TRUE} - remove \code{NA}s
+#'
+#' @return Returns a vector of summary statistics for \code{x}
+#' @export
+#'
+#' @examples
 statSummary <- function(x, na.rm = TRUE){
-  
+
   xMean <- mean(x, na.rm = na.rm)
   xVar <- var(x, na.rm = na.rm)
   xRange <- range(x, na.rm = ra.rm)
-  
+
   c("Min" = xRange[1],
     "Mean" = xMean,
     "Variance" = xVar,
     "Max" = xRange[2])
-  
+
 }
